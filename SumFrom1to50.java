336aa0882d09a00fdb0ef4ee741ad69659e6911c
Anuraaga Nath <114306656+AnuraagaNath@users.noreply.github.com>
2022-09-28 18:45:27 +0530

    Create SumFrom1to50.java

---
+++ SumFrom1to50.java
@@ -0,0 +1,9 @@
+public class practice{
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1 ; i <= 50; i++){
+            sum = sum+i;
+        }
+        System.out.println("Sum of all natural number from 1 to 50: " + sum);
+    }
+}
